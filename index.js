ee3e1690c2cf0c4aba76aab62ce5a26d19d8faf0
Rosa <164552124+rosamrcl@users.noreply.github.com>
2024-09-13 17:19:59 -0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const urlbasica= 'https://jsonplaceholder.typicode.com/posts'
+
 function teste(){
-fetch('https://jsonplaceholder.typicode.com/posts/1')
+fetch(urlbasica+'/1')
   .then((response) => response.json())
   .then((json) => {
     const titulo = document.getElementById ("titulo");
@@ -10,3 +12,22 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
 }
 teste();
 
+function enviar(){
+    const titulo=document.getElementById ('exampleFormControlInput1');
+    const post=document.getElementById ('exampleFormControlTextarea1')
+    fetch(urlbasica, {
+        method: 'POST',
+        body: JSON.stringify({
+          title: titulo,
+          body: post,
+          userId: 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+
+}
+enviar();
